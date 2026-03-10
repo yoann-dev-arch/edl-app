974bc1ba7797fa079de5e1c218386c3012b9215a
Yoann <yoann.tibi36 @gmail.com>
2026-03-10 22:02:31 +0100

    app EDL v1

---
+++ sw.js
@@ -0,0 +1,16 @@
+const CACHE_NAME = 'edl-app-v1';
+const URLS_TO_CACHE = ['./index.html'];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(URLS_TO_CACHE))
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
